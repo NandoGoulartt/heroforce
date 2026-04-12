8cb2fd534e56e2d390afac1a8273b7fe99875c1b
Luiz Fernando <nandogoulart.m@gmail.com>
2026-04-11 22:21:29 -0300

    feature: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
